cb08fe43221105633bcca7a02c55bd325a8543de
Vijay desai <desaivijay918@gmail.com>
2023-03-29 14:02:30 -0400

    created new activity for add contact

--- app/src/main/java/com/vijay/contactlist/MainActivity.kt
+++ app/src/main/java/com/vijay/contactlist/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.vijay.contactlist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vijay.contactlist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.addcontact.setOnClickListener {
+            startActivity(Intent(this, AddContact:: class.java))
+        }
     }
 }
